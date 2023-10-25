892c88e0141843b65f2d7cc930215fae4ab02195
YumanGong <gongymsilvia@gmail.com>
2023-10-24 23:19:15 -0500

    update

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ const Cereal = require('./cereal.js');
 const Milk = require('./milk.js');
 const Dessert = require('./dessert.js');
 const Meal = require('./meal.js');
+
+
+let mondayBowl = new Cereal("Cinnamon Toast Crunch",
+  ["Cinnamon",
+    "Toast",
+    "Sugar",
+    "Crunch",
+    "MSG"
+  ], 8.95, true, false);
+console.log(mondayBowl);
+
+let milk = new Milk("brand", "flavor");
+console.log(milk);
